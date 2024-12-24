c450873ec5a4299b83d19f81402f7d09c5b9feb9
schwienernitzel <pfelix0803@gmail.com>
2024-12-24 11:14:02 +0000

    liszt: Include goldfish repository
    Signed-off-by: schwienernitzel <pfelix0803@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -10,5 +10,12 @@ cd ${THISDIR}/device/huawei/liszt/patches
 ./install.sh
 cd ${THISDIR}
 
+# Clone goldfish repository
+if [ ! -d "device/generic/goldfish" ]; then
+    git clone https://github.com/liszt-dev/android_device_generic_goldfish -b cm-14.1 device/generic/goldfish
+else
+    echo "Detected 'device/generic/goldfish' - Skipping..."
+fi
+
 # Add lunch combo options
 add_lunch_combo omni_liszt-userdebug
